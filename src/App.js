09e8ae523e63ccd063ce0bf3f33a7311484b1b9c
Kajal Singh <singh.kajal940@gmail.com>
2020-08-04 00:23:31 +0100

    uuid installed and imported to generate random ids

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Header from  './components/layout/Header';
 import Todos from './components/Todos.js';
 import AddTodo from './components/AddTodo.js';
+import {v4 as uuid}  from 'uuid';
+//for generating random ids after installing uuid in terminal
 
 
 import './App.css';
@@ -10,17 +12,18 @@ class App extends Component {
   state ={
     todos: [
       {
-        id: 1,
+        // doing this will generate random ids
+        id: uuid(), 
         title: 'Breaksfast',
         completed: false
       },
       {
-        id: 2,
+        id: uuid(),
         title: 'Baking',
         completed: true
       },
       {
-        id: 3,
+        id: uuid(),
         title: 'Laundry',
         completed: false
       }
@@ -43,13 +46,25 @@ delTodo = (id) =>{
     !== id)] });
 }
 
+
+//Add Todo
+addTodo = (title) => {
+  // console.log(title)
+  const newTodo = {
+    id: uuid(),
+    title,
+    completed: false
+  }
+  this.setState({ todos: [...this.state.todos, newTodo] });
+}
+
   render(){
     // console.log(this.state.todos)
   return (
     <div className="App">
       <div className="container">
         <Header />
-        < AddTodo />
+        < AddTodo addTodo={this.addTodo} />
         <Todos todos={this.state.todos} markComplete={this.markComplete}
         delTodo={this.delTodo} />
        </div>
