4174a28a1c1bf33d34cc3c5350f5fe232fb9a277
Kajal Singh <singh.kajal940@gmail.com>
2020-08-03 23:05:45 +0100

    Initial commit

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 
 export class TodoItem extends Component {
+    getStyle = () => {
+
+        return{
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ?
+            'line-through' : 'none'
+        }
+
+        //Dynamic styling
+        /*if(this.props.todo.completed){
+            return{
+                textDecoration:'line-through'
+            }
+        }else{
+            return{
+                textDecoration:'none'
+            }
+        }
+        */
+    }
+
+    markComplete = (e) => {
+        console.log(this.props);
+    }
+
     render() {
         return (
-            <div>
-                <p>{this.props.todo.title }</p>
+            <div style={ this.getStyle()}>
+                <p>
+                    <input type="checkbox" onChange={this.markComplete} /> {' '}
+                    {this.props.todo.title }
+                </p>
             </div>
         )
     }
 }
+
+//Proptypes
+TodoItem.propTypes ={
+    todo: PropTypes.object.isRequired
+}
+
+
+// const itemStyle ={
+//     backgroundColor:'#f4f4f4'
+// }
 
 export default TodoItem
