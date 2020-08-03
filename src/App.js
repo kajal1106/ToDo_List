12059f95655e06fcc2a9438f0426ef17875565af
Kajal Singh <singh.kajal940@gmail.com>
2020-08-03 22:16:41 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Todos from './components/Todos.js';
 
 import './App.css';
 
-function App() {
+class App extends Component {
+  state ={
+    todos: [
+      {
+        id: 1,
+        title: 'Breaksfast',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Baking',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Laundry',
+        completed: false
+      }
+    ]
+  }
+  render(){
+    console.log(this.state.todos)
   return (
     <div className="App">
-      <Todos />
+      <Todos todos={this.state.todos} />
     </div>
   );
+}
 }
 
 export default App;
